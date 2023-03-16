60c59c274cf58bc4379e604fb515945471cb1700
alex4doorow <alex4doorow@gmail.com>
2023-03-16 12:58:46 +0300

    sobes: links

---
+++ sir-richard-boss-samples/src/main/java/com/sir/richard/boss/y/sobes/MainSobes.java
@@ -0,0 +1,6 @@
+package com.sir.richard.boss.y.sobes;
+
+public class MainSobes {
+    // 50 вопросов https://github.com/jmorozov/awesome-java-backend-interview-rus
+    // 60 вопросов https://habr.com/ru/post/485678/
+}
