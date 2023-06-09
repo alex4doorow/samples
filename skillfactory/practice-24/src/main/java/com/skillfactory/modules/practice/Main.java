a544e4cd7546c4288079f6a46864d31afe5fb790
alex4doorow <alex4doorow@gmail.com>
2023-06-09 09:29:29 +0300

    24.7 practice: maven + poi + enum + getter|setter + override: minor

--- skillfactory/practice-24/src/main/java/com/skillfactory/modules/practice/Main.java
+++ skillfactory/practice-24/src/main/java/com/skillfactory/modules/practice/Main.java
@@ -1,16 +1,21 @@
 package com.skillfactory.modules.practice;
 
+import com.github.javafaker.Faker;
 import com.skillfactory.modules.practice.model.Student;
 import com.skillfactory.modules.practice.model.University;
 import com.skillfactory.modules.practice.type.StudyProfile;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        Faker faker = new Faker();
+
         University mgu = new University("mgu", "MGU", "MGU", 1789,  StudyProfile.PROGRAMMING);
         University mvtu = new University("mvtu", "MVTU", "MVTU", 1789,  StudyProfile.ASTRONOMY);
 
-        Student elonMask = new Student("Elon Mask", mgu.getId(), 5, 100);
-        Student steveJobs = new Student("Steve Jobs", mvtu.getId(), 3, 15);
+        Student elonMask = new Student(faker.name().fullName(), mgu.getId(), 5, 100);
+        Student steveJobs = new Student(faker.name().fullName(), mvtu.getId(), 3, 15);
 
         System.out.println(mgu);
         System.out.println(mvtu);
